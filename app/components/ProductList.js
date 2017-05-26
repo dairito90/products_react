7aeccf8db98a3f06d3a54c9fc2ffecf763989a2b
Dairon Rodriguez <drodriguez@fvi.edu>
2017-05-25 21:55:28 -0400

    progress but still err

--- app/components/ProductList.js
+++ app/components/ProductList.js
@@ -1,34 +1,42 @@
 import React from 'react';
 
-function Product (props) {
+function Product(props) {
+
     return (
-        <div className="items">
-            <div className="item">
-                <div className="content">
-                    <div className="name">
-                        <p>Product {props.name} goes here!</p>
-                    </div>
-                    <div className="description">
-                        <p>Product description</p>
-                    </div>
-                    <div className="extra">
-                        <p>Just Extra!!!</p>
-                    </div>
+
+        <div className="item">
+            <div className="image">
+                <img src={this.props.productImageUrl}/>
+            </div>
+            <div className="content">
+                <div className="header">
+                    <a>
+                        <i className="caret"/>
+                    </a>
                 </div>
-           </div>
+                <div className="description">
+                    <a href={this.props.url}></a>
+                    <p>{this.props.description}</p>
+                </div>
+                <div className="extra">
+                    <span>Submitted by</span>
+                    <img className="avatar image" src={this.props.submitterAvatarUrl}/>
+                </div>
+            </div>
         </div>
+
     );
 }
 
-
 class ProductList extends React.Component {
-    render () {
+
+    render() {
+        const product = Seed.product[0];
         return (
             <div>
-            <Product
-                name='Product 1'
-            />
+                <Product id={product.id} title={product.title} description={product.description} url={product.url} votes={product.votes} submitterAvatarUrl={product.submitterAvatarUrl} productImageUrl={product.productImageUrl}/>
             </div>
+
         );
     }
 }
