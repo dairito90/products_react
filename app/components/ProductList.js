419f24b35fc43e98f0295fd14c389328c0224e2a
Dairon Rodriguez <drodriguez@fvi.edu>
2017-05-31 21:11:20 -0400

    pruducts list  done

--- app/components/ProductList.js
+++ app/components/ProductList.js
@@ -1,40 +1,72 @@
 import React from 'react';
 import { products, generateVoteCount } from '../seed';
 
-function Product(props) {
+class Product extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+
+  handleUpVote(){
+    this.props.onVote(this.props.id)
+  }
+
+  render(){
   return (
-      <div className='ui unstackable items'>
+      <div className='items'>
         <div className='image'>
-          <img src={props.productImageUrl} />
+          <img  className="productimg"
+              src={this.props.productImageUrl} />
         </div>
         <div className='content'>
           <div className='header'>
-            <a>
-              <i className='caret' />
+            <a onClick = {this.handleProductUpVote}>
+              {/* <img src={this.props.productImageUrl} /> */}
             </a>
+            {this.props.votes}
           </div>
           <div className='description'>
-            <a href={props.url}></a>
-            <p>{props.description}</p>
+            <a href={this.props.url}>
+              {this.props.title}
+            </a>
+            <p>{this.props.description}</p>
           </div>
           <div className='extra'>
             <span>Submitted by:</span>
             <img
               className='avatar image'
-              src={props.submitterAvatarUrl}
+              src={this.props.submitterAvatarUrl}
             />
           </div>
         </div>
       </div>
-  );
+    );
+  }
 }
 
 class ProductList extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+        products:[]
+    }
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  }
+
+  handleProductUpVote(productId){
+    console.log(`${productId} was upvoted`);
+  }
+
   render() {
-    const product = products[0];
-    return (
-      <div>
+    const sortedProducts = products.sort((a, b) =>{
+      b.votes - a.votes
+    });
+    const productComponents = sortedProducts.map((product) => (
         <Product
+          key={'product-' + product.id}
           id={product.id}
           title={product.title}
           description={product.description}
@@ -42,7 +74,12 @@ class ProductList extends React.Component {
           votes={product.votes}
           submitterAvatarUrl={product.submitterAvatarUrl}
           productImageUrl={product.productImageUrl}
+          onVote= {this.handleProductUpVote}
         />
+    ));
+    return (
+      <div className='items'>
+        {productComponents}
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry:'./app/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        rules: [
-            { test: /\.(js)$/, use: 'babel-loader'},
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-        ]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: 'app/index.html'
-    })]
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'app/index.html'
+  })]
 };
